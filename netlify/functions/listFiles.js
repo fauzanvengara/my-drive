46a03331a1da2005ec4375a2831ca4627b2be0ae
Fauzan <fauzutester@gmail.com>
2026-07-19 18:30:59 +0530

    list files js

---
+++ netlify/functions/listFiles.js
@@ -0,0 +1,32 @@
+const { getDrive } = require("./auth");
+
+exports.handler = async () => {
+  try {
+    const drive = await getDrive();
+
+    const folderId = process.env.DRIVE_FOLDER_ID;
+
+    const response = await drive.files.list({
+      q: `'${folderId}' in parents and trashed = false`,
+      fields: "files(id,name,mimeType,size,modifiedTime)",
+      orderBy: "folder,name",
+    });
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(response.data.files),
+    };
+  } catch (err) {
+    console.error(err);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        error: err.message,
+      }),
+    };
+  }
+};

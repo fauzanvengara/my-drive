2516cc14ff4de60433f11c0fe111989cff0c65eb
Fauzan <fauzutester@gmail.com>
2026-07-21 17:10:30 +0530

    Add webViewLink field

--- netlify/functions/downloadFile.js
+++ netlify/functions/downloadFile.js
@@ -1,37 +1,6 @@
-const { google } = require("googleapis");
-const { getDrive } = require("./auth");
-
-exports.handler = async (event) => {
-  try {
-    const fileId = event.queryStringParameters?.id;
-
-    if (!fileId) {
-      return {
-        statusCode: 400,
-        body: "Missing file id"
-      };
-    }
-
-    const drive = await getDrive();
-
-    const file = await drive.files.get({
-      fileId,
-      fields: "webViewLink, webContentLink"
-    });
-
-    return {
-      statusCode: 302,
-      headers: {
-        Location: file.data.webContentLink || file.data.webViewLink
-      },
-      body: ""
-    };
-  } catch (err) {
-    console.error(err);
-
-    return {
-      statusCode: 500,
-      body: err.message
-    };
-  }
+exports.handler = async () => {
+  return {
+    statusCode: 501,
+    body: "Download function will be implemented next."
+  };
 };
